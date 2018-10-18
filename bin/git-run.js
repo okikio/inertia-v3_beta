91fa4efd796c323531efe0389e98dc1a09e18d72
Okiki Ojo <femikiki@gmail.com>
2018-10-18 22:33:30 +0000

    /home/ubuntu/.nvm/versions/node/v6.11.2/bin/node

--- bin/git-run.js
+++ bin/git-run.js
@@ -3,5 +3,5 @@
 var exec = require('child_process').exec;
 var argv = Array.from(process.argv);
 exec("git add -A .", function (err) { if (err) { throw err; } });
-exec("git commit -a -m '" + (argv[1] || "Update") + '\'', function (err) { if (err) { throw err; } });
+exec("git commit -a -m '" + (argv[0] || "Update") + '\'', function (err) { if (err) { throw err; } });
 exec("git push origin master", function (err) { if (err) { throw err; } });
