db6f3f4d5d1e9740956be2cb1ce9762d5d84c11f
Okiki Ojo <femikiki@gmail.com>
2019-01-27 22:27:09 +0000

    Update

--- compile/object.js
+++ compile/object.js
@@ -581,25 +581,20 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                 return _.isNumber(id) ? _.values(obj)[id] :
                         Object.path(obj, id);
             }],
-
             // First Element in an Object
             [["first"], function(obj) { return Object.nth(obj, 0); }],
-
             // Last Element in an Object
             [["last"], function(obj) { return Object.nth(obj, _.size(obj) - 1); }],
-
             // Check if an Object is Array Like
             [["isArrayLike"], function(obj) {
                 var len = _.isNumber(obj.length) && obj.length;
                 return len === 0 || len > 0 && (len - 1) in obj;
             }],
-
             // Clear an Object of all it's contents
             [["remove", "empty"], function(obj) {
                 for (var i in obj) { delete obj[i]; }
                 return obj;
             }],
-
             // Inline Stringify Function
             [["str", "stringify"], function(obj, short) {
                 var fns = [], json;
@@ -611,30 +606,22 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                                 { fns.push(val.toStr ? val.toStr() : val.toString()); return "_"; }
                             return val;
                         }, 2);
-    
                         json = json.replace(/\"_\"/g,
                             function($) { return fns.shift(); });
                     } else { json = "The arguments for method `[str, stringify]` is not defined."; }
                     return json;
                 } catch (e) { return "JSON Error (" + e.message + ")"; }
             }],
-
             // Clone An Object
             [["clone", "copy"], function(obj) {
                 return $JSON.parse($JSON.stringify(
                         _.extend.apply({}, [{}]
                             .concat(Array.from(arguments)))));
             }],
-
             // Finds value of a path in an Object
             [["path", "prop"], Util.path],
-            
             // A more efficient `new` keyword that allows for arrays to be passed as Arguments
-            [["new"], Native("ctor", "args",
-                "var F = function() { return ctor.apply(this, args); };" +
-                "F.prototype = ctor.prototype;" +
-                "return new F")],
-
+            [["new"], Util.new],
             // Prev Value in Object
             [["prev"], function(obj) {
                 return $indx > 0 ? {
@@ -643,19 +630,16 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                     done: false
                 } : { done: true };
             }],
-
             // Even Indexed Values in an Object
             [["even"], function(obj) {
                     return Object.filter(obj,
                         function(val, i) { return i % 2 === 1; });
             }],
-
             // Odd Indexed Values in an Object
             [["odd"], function(obj) {
                 return Object.filter(obj,
                     function(val, i) { return i % 2 === 0; });
             }],
-
             // Next Value in Object
             [["next"], function(obj) {
                 return $indx < _.size(obj) ? {
@@ -664,13 +648,12 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                     done: false
                 } : { done: true };
             }],
-            
             // Sum of all values in an Object
             [["sum"], function (obj, id, deep) {
                 return Object.reduce(obj, function (acc, val, i) {
                     var _val;
-                    if (Inertia.isDef(id)) {
-                        _val = Object.path(obj, Inertia.toArray(i + "." + id));
+                    if ($in.isDef(id)) {
+                        _val = Object.path(obj, $in.toArray(i + "." + id));
                     } else if (_.isObject(val) || _.isArray(val)) {
                         _val = deep ? Object.sum(val, id, deep) : 0;
                     } else if (_.isFunction(id)) {
@@ -680,7 +663,6 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                     return acc;
                 }, 0);
             }],
-            
             // Average
             [["average", "avg"], function (obj, id) {
                 if (Inertia.isDef(id)) {
@@ -690,7 +672,6 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                     Inertia.isDef(id) ? Object.pluck(obj, id) : obj,
                 null, true) / Object.size(obj);
             }],
-            
             // Median
             [["median", "med"], function (obj, id) {
                 var _obj = Object.values(obj);
@@ -701,21 +682,17 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                                   _obj[len / 2]) / 2; }
                     return _obj[floor(len / 2)];
                 }
-            
                 if (len % 2 === 0) 
                     { return (_obj[(len / 2) - 1][id] + 
                               _obj[len / 2][id]) / 2; }
-            
                 return _obj[floor(len / 2)][id];
             }],
-            
             // Find the Mode (the most repeated element) of an Object
             [["mode", "repeated"], function (obj, id) {
                 return Object.countBy(
                     $in.isDef(id) ? Object.pluck(obj, id) : obj
                 ).pairs().max(Object.last).head();
             }],
-            
             // Split an Object into smaller pieces 
             [["chunk"], function(obj, size, split) {
                 var chunks = [], index = 0;
@@ -723,7 +700,6 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                     chunks = Object.chunk(obj.split(split || ""), size);
                 } else if (Array.isArray(obj) || typeof obj === 'object') {
                     var _keys = Object.keys(obj), _obj, Chunkkeys;
-                    
                     // Used a function outside, can't define a function in a loop :(
                     var fn = function () {
                         Chunkkeys = _keys.slice(index, index + size);
@@ -732,7 +708,6 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                             _obj[_key] = obj[_key]; 
                         }, obj);
                     };
-                    
                     do { 
                         if (!Array.isArray(obj)) { fn(); } 
                         else { _obj = obj.slice(index, index + size); }
@@ -741,23 +716,19 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                 } else { chunks.push([obj]); }
                 return chunks;
             }], 
-            
             // Dump all values at a certain point in the Object
             [["dump", "log"], function (obj) { Core.log.apply(obj, arguments); return obj; }],
-            
             // Set prototype methods quickly
             [["macro"], function (obj, name, fn) { 
                 obj.prototype[name] = fn || Core.Fn("return this;"); 
                 return obj; 
             }],
-            
             // Instantiates the given class with Object values as a constructor
             [["mapInto"], function (obj, _class) { 
                 return Object.map(obj, function (val, i) { 
                     return new _class(val, i);
                 });
             }],
-            
             // Returns the items in an Object with specified keys
             [["only"], function (obj) {
                 var _args = args(arguments, 1);
@@ -767,18 +738,15 @@ var Inertia = {}, $in, Define, require; // Inertia Entry Point
                         return props.indexOf(item) !== -1; 
                     });
                 }
-                
                 return Object.reduce(obj, function(acc, val, i) {
                     if (props.indexOf(i) !== -1) { acc[i] = obj[i]; }
                     return acc;
                 }, {});
             }],
-            
             // Passes the Object to the given callback and returns the result
             [["pipe"], function (obj, fn) 
                 { return (fn || Core.Fn("v", "return v;")) (obj); }]
         ]);
-        
         // Extend Methods
         MapFunc(Native, $Map);
         MapFunc(Native.prototype, $Map, true);
--- modules/object/index.js
+++ modules/object/index.js
@@ -16,25 +16,20 @@
                 return _.isNumber(id) ? _.values(obj)[id] :
                         Object.path(obj, id);
             }],
-
             // First Element in an Object
             [["first"], function(obj) { return Object.nth(obj, 0); }],
-
             // Last Element in an Object
             [["last"], function(obj) { return Object.nth(obj, _.size(obj) - 1); }],
-
             // Check if an Object is Array Like
             [["isArrayLike"], function(obj) {
                 var len = _.isNumber(obj.length) && obj.length;
                 return len === 0 || len > 0 && (len - 1) in obj;
             }],
-
             // Clear an Object of all it's contents
             [["remove", "empty"], function(obj) {
                 for (var i in obj) { delete obj[i]; }
                 return obj;
             }],
-
             // Inline Stringify Function
             [["str", "stringify"], function(obj, short) {
                 var fns = [], json;
@@ -46,30 +41,22 @@
                                 { fns.push(val.toStr ? val.toStr() : val.toString()); return "_"; }
                             return val;
                         }, 2);
-    
                         json = json.replace(/\"_\"/g,
                             function($) { return fns.shift(); });
                     } else { json = "The arguments for method `[str, stringify]` is not defined."; }
                     return json;
                 } catch (e) { return "JSON Error (" + e.message + ")"; }
             }],
-
             // Clone An Object
             [["clone", "copy"], function(obj) {
                 return $JSON.parse($JSON.stringify(
                         _.extend.apply({}, [{}]
                             .concat(Array.from(arguments)))));
             }],
-
             // Finds value of a path in an Object
             [["path", "prop"], Util.path],
-            
             // A more efficient `new` keyword that allows for arrays to be passed as Arguments
-            [["new"], Native("ctor", "args",
-                "var F = function() { return ctor.apply(this, args); };" +
-                "F.prototype = ctor.prototype;" +
-                "return new F")],
-
+            [["new"], Util.new],
             // Prev Value in Object
             [["prev"], function(obj) {
                 return $indx > 0 ? {
@@ -78,19 +65,16 @@
                     done: false
                 } : { done: true };
             }],
-
             // Even Indexed Values in an Object
             [["even"], function(obj) {
                     return Object.filter(obj,
                         function(val, i) { return i % 2 === 1; });
             }],
-
             // Odd Indexed Values in an Object
             [["odd"], function(obj) {
                 return Object.filter(obj,
                     function(val, i) { return i % 2 === 0; });
             }],
-
             // Next Value in Object
             [["next"], function(obj) {
                 return $indx < _.size(obj) ? {
@@ -99,13 +83,12 @@
                     done: false
                 } : { done: true };
             }],
-            
             // Sum of all values in an Object
             [["sum"], function (obj, id, deep) {
                 return Object.reduce(obj, function (acc, val, i) {
                     var _val;
-                    if (Inertia.isDef(id)) {
-                        _val = Object.path(obj, Inertia.toArray(i + "." + id));
+                    if ($in.isDef(id)) {
+                        _val = Object.path(obj, $in.toArray(i + "." + id));
                     } else if (_.isObject(val) || _.isArray(val)) {
                         _val = deep ? Object.sum(val, id, deep) : 0;
                     } else if (_.isFunction(id)) {
@@ -115,7 +98,6 @@
                     return acc;
                 }, 0);
             }],
-            
             // Average
             [["average", "avg"], function (obj, id) {
                 if (Inertia.isDef(id)) {
@@ -125,7 +107,6 @@
                     Inertia.isDef(id) ? Object.pluck(obj, id) : obj,
                 null, true) / Object.size(obj);
             }],
-            
             // Median
             [["median", "med"], function (obj, id) {
                 var _obj = Object.values(obj);
@@ -136,21 +117,17 @@
                                   _obj[len / 2]) / 2; }
                     return _obj[floor(len / 2)];
                 }
-            
                 if (len % 2 === 0) 
                     { return (_obj[(len / 2) - 1][id] + 
                               _obj[len / 2][id]) / 2; }
-            
                 return _obj[floor(len / 2)][id];
             }],
-            
             // Find the Mode (the most repeated element) of an Object
             [["mode", "repeated"], function (obj, id) {
                 return Object.countBy(
                     $in.isDef(id) ? Object.pluck(obj, id) : obj
                 ).pairs().max(Object.last).head();
             }],
-            
             // Split an Object into smaller pieces 
             [["chunk"], function(obj, size, split) {
                 var chunks = [], index = 0;
@@ -158,7 +135,6 @@
                     chunks = Object.chunk(obj.split(split || ""), size);
                 } else if (Array.isArray(obj) || typeof obj === 'object') {
                     var _keys = Object.keys(obj), _obj, Chunkkeys;
-                    
                     // Used a function outside, can't define a function in a loop :(
                     var fn = function () {
                         Chunkkeys = _keys.slice(index, index + size);
@@ -167,7 +143,6 @@
                             _obj[_key] = obj[_key]; 
                         }, obj);
                     };
-                    
                     do { 
                         if (!Array.isArray(obj)) { fn(); } 
                         else { _obj = obj.slice(index, index + size); }
@@ -176,23 +151,19 @@
                 } else { chunks.push([obj]); }
                 return chunks;
             }], 
-            
             // Dump all values at a certain point in the Object
             [["dump", "log"], function (obj) { Core.log.apply(obj, arguments); return obj; }],
-            
             // Set prototype methods quickly
             [["macro"], function (obj, name, fn) { 
                 obj.prototype[name] = fn || Core.Fn("return this;"); 
                 return obj; 
             }],
-            
             // Instantiates the given class with Object values as a constructor
             [["mapInto"], function (obj, _class) { 
                 return Object.map(obj, function (val, i) { 
                     return new _class(val, i);
                 });
             }],
-            
             // Returns the items in an Object with specified keys
             [["only"], function (obj) {
                 var _args = args(arguments, 1);
@@ -202,18 +173,15 @@
                         return props.indexOf(item) !== -1; 
                     });
                 }
-                
                 return Object.reduce(obj, function(acc, val, i) {
                     if (props.indexOf(i) !== -1) { acc[i] = obj[i]; }
                     return acc;
                 }, {});
             }],
-            
             // Passes the Object to the given callback and returns the result
             [["pipe"], function (obj, fn) 
                 { return (fn || Core.Fn("v", "return v;")) (obj); }]
         ]);
-        
         // Extend Methods
         MapFunc(Native, $Map);
         MapFunc(Native.prototype, $Map, true);
