367d1dbd83429d2f5b29f80c0f9649fc71ef79f3
Okiki Ojo <femikiki@gmail.com>
2019-04-04 22:23:08 +0000

    Update

--- src/class.js
+++ src/class.js
@@ -144,7 +144,7 @@
                     _freed: [], // For creating more efficient Classes
                     // Creates efficient new Classes
                     new: function (arg) {
-                        var _class;
+                        var _class; arg = arg.length ? arg : [arg]
                         if (Class._freed.length > 0) {
                             _class = Class._freed.pop();
                             _class.init.apply(_class, arg);
